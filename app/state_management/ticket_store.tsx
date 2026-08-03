a5e173fbc1df6fb24f58748f4c2e1dbd07424b20
ishu17077 <ishu17077@users.noreply.github.com>
2026-08-03 23:04:13 +0530

    Change: ticket default avaibility from false to true

--- app/state_management/ticket_store.tsx
+++ app/state_management/ticket_store.tsx
@@ -12,7 +12,7 @@ const TicketContext = createContext<TicketStore | null>(null)
 
 function createTicketStore() {
     return create<TicketState>(() => ({
-        isAvailable: (process.env.NEXT_PUBLIC_TICKET_AVAILABLE ?? '').toLowerCase() === 'true',
+        isAvailable: (process.env.NEXT_PUBLIC_TICKET_AVAILABLE ?? 'true').toLowerCase() === 'true',
         redirectUrl: (process.env.NEXT_PUBLIC_TICKET_REDIRECT_URL ?? "https://innovatexcom.xyz")
     }))
 }
