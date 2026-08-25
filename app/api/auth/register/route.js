2989c266fb9f93a9a287aa43eaac5f5a58d81e35
Lone Wolf <51350439+ishu17077@users.noreply.github.com>
2026-08-25 14:05:15 +0530

    Update route.js

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -27,7 +27,7 @@ import {
 } from "../../constants";
 
 export const POST = asyncCacheHandler(asyncDbHandler(async (req) => {
-  if (isTicketAvailable) {
+  if (!isTicketAvailable) {
     const error = new Error("Registrations closed. Thank you for your cooperation")
     error.statusCode = 403
     throw error
